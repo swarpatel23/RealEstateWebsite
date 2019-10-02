8ccb347921b93f995cf55ca7c3d503b0054cafe7
swarpatel23 <smp1613@gmail.com>
2019-10-02 18:02:38 +0530

    sigin signup updated

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,EventEmitter, Output} from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+
 import * as $ from 'jquery';
 
 
@@ -9,44 +12,77 @@ import * as $ from 'jquery';
 })
 export class EntryComponent implements OnInit {
 
-  constructor() { }
+  public username:string;
+  public password:string;
+  public contactno:number;
+  public address:string;
+  public email:string;
+  
+  @Output() public childevent =new EventEmitter();
+
+  signin(){
+      this.childevent.emit(true)
+  }
+
+  signup(){
+    this.childevent.emit(true)
+  }
+  constructor(private route: ActivatedRoute) {
+    this.route.url.subscribe(url => {
+      //console.log(url);
+      this.chk();
+    });
+  }
+
+  chk() {
+    let disp = this.route.snapshot.paramMap.get("disp")
+    if (disp == "signup") {
+      $('.signin').hide();
+      $('.signup').show();
+    }
+    else {
+      $('.signup').hide();
+      $('.signin').show();
+    }
+
+  }
 
   ngOnInit() {
     let target;
-    let searchParams = new URLSearchParams(window.location.search);
-    if(searchParams.has('disp'))
-    {
-        let param = searchParams.get('disp')
-        if(param=="signin")
-        {
-            
-            $('.signup').hide();
-            $('.signin').show();
-        }
-        if(param=="signup")
-        {
-            $('.signin').hide();
-            $('.signup').show();
-        }
-    }
+    // let searchParams = new URLSearchParams(window.location.search);
+    // if(searchParams.has('disp'))
+    // {
+    //     let param = searchParams.get('disp')
+    //     if(param=="signin")
+    //     {
+
+    //         $('.signup').hide();
+    //         $('.signin').show();
+    //     }
+    //     if(param=="signup")
+    //     {
+    //         $('.signin').hide();
+    //         $('.signup').show();
+    //     }
+    // }
+
+
     $('.link a').on('click', function (e) {
-        e.preventDefault();
-        target = $(this).attr('href');
-        
-        //$(target).show(1000);
-        console.log(target);
-        if(target=="#signin")
-        {
-            $('.signup').hide(100);
-            $('.signin').fadeIn(700);
-            document.title="signin";
-        }
-        else
-        {
-            $('.signin').hide();
-            $('.signup').fadeIn(700);
-            document.title="signup";
-        }
+      e.preventDefault();
+      target = $(this).attr('href');
+
+      //$(target).show(1000);
+      console.log(target);
+      if (target == "#signin") {
+        $('.signup').hide(100);
+        $('.signin').fadeIn(700);
+        document.title = "signin";
+      }
+      else {
+        $('.signin').hide();
+        $('.signup').fadeIn(700);
+        document.title = "signup";
+      }
     });
   }
 
